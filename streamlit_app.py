25c29b7eaa1085fbd0e0bff39fb7e847a88943c9
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-07 13:39:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ import streamlit as st
 tab1, tab2, tab3 = st.columns(3)
 
 # Define a button to switch to the next tab
-if tab1.button("Next Tab"):
+if tab1.button("Next Tab", key = 'a'):
     tab1.empty()
     tab2.write("This is the content of Tab 2.")
-elif tab2.button("Next Tab"):
+elif tab2.button("Next Tab", key = 'b'):
     tab2.empty()
     tab3.write("This is the content of Tab 3.")
-elif tab3.button("Next Tab"):
+elif tab3.button("Next Tab", key = 'c'):
     tab3.empty()
     tab1.write("This is the content of Tab 1.")
 else:
