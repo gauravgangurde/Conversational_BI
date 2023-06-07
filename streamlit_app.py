8872f94626d7d8ab276ae21bfb3cc25f4e479a4c
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-07 13:25:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,34 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from PIL import Image
-import openai
-import json
-import ast
-import csv
 
-def dicts_to_csv(list_of_dicts, filename):
-    keys = list_of_dicts[0].keys()
+def main():
+    tabs = ["Tab 1", "Tab 2", "Tab 3"]
+    current_tab = st.sidebar.radio("Select a tab", tabs)
+    
+    if current_tab == "Tab 1":
+        tab1()
+    elif current_tab == "Tab 2":
+        tab2()
+    elif current_tab == "Tab 3":
+        tab3()
 
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=keys, delimiter='|')
-        writer.writeheader()
-        writer.writerows(list_of_dicts)
-	
-	
-image = Image.open('exl.png')
-	
-with st.sidebar:
-	st.image(image, width = 150)
-	st.header('Generative AI')
+def tab1():
+    st.title("Tab 1")
+    st.write("Content for Tab 1")
+    next_tab_button("Tab 2")
 
+def tab2():
+    st.title("Tab 2")
+    st.write("Content for Tab 2")
+    next_tab_button("Tab 3")
 
-st.header("Test")
+def tab3():
+    st.title("Tab 3")
+    st.write("Content for Tab 3")
+    next_tab_button("Tab 1")
 
-response = """{ "mails": [ { "name": "Agent 1", "performance category": "Consistent achiever", "mail": "Dear Agent 1,\n\nI am writing to provide you with feedback on your latest performance data. It is great to see that you have consistently achieved your targets and even exceeded them last month. Keep up the good work!\n\nHowever, I would suggest that you focus on improving your communication skills with clients. This can help you build better relationships and increase sales.\n\nHere are two articles that can help you improve your sales skills:\n1. https://hbr.org/2017/06/the-best-salespeople-are-deeply-empathetic\n2. https://www.salesforce.com/blog/2017/01/5-sales-tips-from-people-who-hate-sales.html\n\nThank you for your hard work.\n\nBest regards,\nManager" }, { "name": "Agent 5", "performance category": "Consistent achiever", "mail": "Dear Agent 5,\n\nI am writing to provide you with feedback on your latest performance data. It is great to see that you have consistently achieved your targets and even exceeded them last month. Keep up the good work!\n\nHowever, I would suggest that you focus on improving your time management skills. This can help you increase your productivity and achieve even better results.\n\nHere are two training links that can help you improve your sales skills:\n1. https://www.udemy.com/course/time-management-salespeople/\n2. https://www.carew.com/time-management-sales-training/\n\nThank you for your hard work.\n\nBest regards,\nManager" }, { "name": "Agent 9", "performance category": "Consistent achiever", "mail": "Dear Agent 9,\n\nI am writing to provide you with feedback on your latest performance data. It is great to see that you have consistently achieved your targets and even exceeded them last month. Keep up the good work!\n\nHowever, I would suggest that you focus on improving your product knowledge. This can help you better understand your clients' needs and offer them more suitable products.\n\nHere are two articles that can help you improve your sales skills:\n1. https://www.salesforce.com/blog/2015/04/7-tips-to-boost-your-product-knowledge.html\n2. https://www.business.com/articles/5-ways-to-improve-your-product-knowledge-for-sales/\n\nThank you for your hard work.\n\nBest regards,\nManager" } ] }"""
-res = ast.literal_eval(response.replace('\n','\\n'))
-for i in res.values():
-	st.markdown(i)
-st.markdown(res["mails"][0]['mail'])
+def next_tab_button(next_tab):
+    col1, col2, col3 = st.beta_columns(3)
+    col2.button("Next Tab", key=next_tab)
 
-for i in res.keys():
-	df = (pd.DataFrame.from_dict(res[i]))
-	dicts_to_csv(res[i], 'mails_data.csv')
-	st.dataframe(pd.read_csv('mails_data.csv', sep = '|'))
-
+if __name__ == "__main__":
+    main()
